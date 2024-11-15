3a07954a44cdb73fb148228328f53272c510753a
Azamatjon-developer <azamatergasev01@gmail.com>
2024-11-15 18:05:28 +0500

    updateCode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
-async function fetchData () {
-  const res = await fetch ("https://jsonplaceholder.typicode.com/photos");
+import Link from "next/link"
+
+async function fetchData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   const result = await res.json()
   return result
 }
-
-export default async function Home () {
-  const res = await fetchData()
-  console.log(res)
+interface post {
+  userId : number;
+  id:number;
+  title: string;
+  body: string;
+}
+export default async function Home() {
+  const posts = await fetchData()
+  console.log(posts.length)
   return (
-    <div>
-      <h2 className="text-[32px] text-slate-700">Main page </h2>
-      <p>
-        {res.title}
-      </p>
+    <div className="min-h-screen bg-gradient-to-b from-gray-800 to-black text-white py-10 px-6">
+      <h2 className="text-4xl font-bold text-center mb-8 text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+        Main Page
+      </h2>
+        <div>
+          {posts?.map((item:post) => (
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+              <Link className="pt-[6px] pl-[12px] pb-[4px] pr-[22px] bg-green-500 mt-10 rounded-md" href={"/post/" + item.id}>See more </Link>
+              <hr />
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
