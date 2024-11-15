d4244b4edd4eb094de46ce3662ae209d962efaf1
Azamatjon-developer <azamatergasev01@gmail.com>
2024-11-15 11:12:13 +0500

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+
+const page = () => {
+  return (
+    <div className='bg-[#222] text-white h-screen overflow-y-auto'>
+      <Header/>
+      <h2>Hello </h2>
+    </div>
+  )
+}
+
+export default page
