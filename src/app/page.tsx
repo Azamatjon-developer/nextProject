1051d09625e83a69b7ff52d69832b9c981071803
Azamatjon-developer <azamatergasev01@gmail.com>
2024-11-17 14:26:19 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-async function fetchData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const result = await res.json()
-  return result
+interface MetaDataProps {
+  params: { id: number };
+  searchParams?: string | number;
 }
 
 interface Post {
-  userId: number
-  id: number
-  title: string
-  body: string
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+// Function to fetch all posts or a specific post by ID
+async function fetchData(id?: number): Promise<Post | Post[]> {
+  const url = id
+    ? `https://jsonplaceholder.typicode.com/posts/${id}`
+    : 'https://jsonplaceholder.typicode.com/posts/';
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+export async function generateMetaData({ params }: MetaDataProps) {
+  const post = (await fetchData(params.id)) as Post;
+  return {
+    title: post.title,
+  };
 }
 
 export default async function Home() {
-  const posts = await fetchData()
+  const posts = (await fetchData()) as Post[];
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-800 to-black text-white py-10 px-6">
@@ -22,7 +43,7 @@ export default async function Home() {
         Main Page
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {posts?.map((item: Post) => (
+        {posts.map((item) => (
           <div
             key={item.id}
             className="bg-gray-900 rounded-lg p-6 shadow-lg hover:shadow-xl transition-shadow duration-300"
@@ -34,7 +55,7 @@ export default async function Home() {
               {item.body.substring(0, 200)}...
             </p>
             <Link
-              href={'/post/' + item.id}
+              href={{ pathname: `/post/${item.id}` }}
               className="inline-block bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300"
             >
               See More
@@ -43,5 +64,5 @@ export default async function Home() {
         ))}
       </div>
     </div>
-  )
+  );
 }
